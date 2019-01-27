af6db1510c9f676a711cc1e264b98df3191e87be
jonatanpersson123 <jonatanpersson123@gmail.com>
2019-01-27 17:32:04 +0100

    Education page added.

---
+++ frontend/src/components/Education.vue
@@ -0,0 +1,46 @@
+<template>
+    <section id="education">
+        <h1>Education</h1>
+        <timeline-elements :elementsData="educationList"></timeline-elements>
+    </section>
+</template>
+
+<script>
+import TimelineElements from './TimelineElements'
+export default {
+    name: 'Education',
+    components: {
+        TimelineElements
+    },
+    data() {
+        return {
+            educationList: [
+                {
+                    title: 'UI DESIGN PATTERNS',
+                    link: 'https://www.interaction-design.org/courses/ui-design-patterns-for-successful-software',
+                    type: 'Course',
+                    location: 'Interaction design foundation',
+                    description: 'Fundamental UX course with UI design patterns.'
+                },
+                {
+                    title: 'GRAPHIC DESIGN',
+                    link: 'www.lth.com',
+                    type: 'Course',
+                    location: 'Lund, Sweden',
+                    description: 'Base course in graphic design. Achieved at Lund University'
+                },
+                {
+                    title: 'BACHELOR DEGREE IN SOFTWARE ENGINEERING',
+                    link: 'www.hkr.se',
+                    type: 'Bachelor programme',
+                    location: 'Kristianstad, Sweden',
+                    description: 'Fundamental programme in software engineering. Graduated with highest honors at Högskolan Kristainstad.'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
