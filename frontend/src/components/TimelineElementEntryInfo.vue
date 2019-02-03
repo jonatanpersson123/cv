3826aa867c7e61d955d22311716101923bfd1095
jonatanpersson123 <jonatanpersson123@gmail.com>
2019-02-03 12:58:25 +0100

    TimelineElementEntryLocation --> TimelineElementEntryInfo renamed.

---
+++ frontend/src/components/TimelineElementEntryInfo.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="info-container flex">
+        <div class="info-icon">
+            <v-icon :name="icon" scale="1.5" />
+        </div>
+        <span>{{text}}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TimelineElementEntryInfo',
+        props: {
+            text: String,
+            icon: String
+        }
+    }
+</script>
+
+<style scoped>
+span {
+    font-size: 16px;
+    color: #363636;
+    margin-left: 8px;
+}
+
+svg {
+    display: block;
+    margin: auto;
+}
+
+.info-container {
+    margin-top: 10px;
+}
+
+.info-icon {
+    display: flex;
+    min-width: 24px;
+}
+
+</style>
