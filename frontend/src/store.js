34527fdd8b5b426be274e2747f265de28b787c8f
jonatanpersson123 <jonatanpersson123@gmail.com>
2019-02-17 23:19:44 +0100

    Scroll removed from store logic.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,23 +2,6 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
 
-var VueScrollTo = require('vue-scrollto')
-
-var options = {
-    container: '#content',
-    easing: 'ease',
-    force: true,
-    cancelable: false,
-    onStart: function(element) {
-    },
-    onDone: function(element) {
-    },
-    onCancel: function() {
-    },
-    x: false,
-    y: true
-}
-
 const chapters = [
     { id: 1, chapterNumber: '01', chapterTitle: 'Intro' },
     { id: 2, chapterNumber: '02', chapterTitle: 'Experience' },
@@ -33,15 +16,17 @@ const initStore = () =>
   (store = new Vuex.Store({
     state: {
         chapters: chapters,
-        currentChapter: chapters[0]
+        currentChapter: chapters[0],
+        isScrolling: false
     },
     mutations: {
       updateCurrentChapter(state, newChapter) {
         state.currentChapter = newChapter
-        VueScrollTo.scrollTo(document.getElementById(newChapter.chapterTitle.toLowerCase()), 650, options)
+      },
+      updateIsScrolling(state, scrolling) {
+        state.isScrolling = scrolling
       }
     },
-
     actions: {}
   }))
 
